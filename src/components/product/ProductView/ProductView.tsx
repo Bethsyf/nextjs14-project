58d3a4c005ddf6962d0a35533bc9f8232138a62b
Bethsyf <bethsyfalcon@gmail.com>
2024-01-30 16:18:43 -0500

    page product and productView

---
+++ src/components/product/ProductView/ProductView.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import { ProductViewItemsOrder } from './ProductViewItemsOrder';
+import styles from './ProductView.module.scss';
+interface ProductViewProps {
+  product: ProductType;
+}
+
+export const ProductView = ({ product }: ProductViewProps) => {
+  return (
+    <main className={styles.ProductView}>
+      <section className={styles.images}>
+        <Image
+          loading="eager"
+          src={product?.image}
+          width={500}
+          height={500}
+          quality={80}
+          alt={product?.title}
+        />
+      </section>
+      <section className={styles.info}>
+        <h1 className={styles.title}>{product?.title}</h1>
+        <p className={styles.category}>{product?.tags}</p>
+        <p className={styles.description}>{product?.description}</p>
+        <span className={styles.price}>$ {product?.price}</span>
+        <ProductViewItemsOrder maxQuantity={product?.quantity} />
+      </section>
+    </main>
+  );
+};
