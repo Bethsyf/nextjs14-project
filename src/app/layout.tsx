8d7e10ee40248069a1f1a984a951f7a8e9cd3de9
Bethsyf <bethsyfalcon@gmail.com>
2024-02-09 09:34:56 -0500

    meta props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <title>Future World</title>
+        <meta
+          name="description"
+          content="Welcome to the future world, an ecommerce from other century"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dmaviub4l/image/upload/v1707489187/shd1faovk8kmygkcsg08.png"
+        />
+        <meta
+          property="og:url"
+          content="https://bethsyf.github.io/portfolio/"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className={`${roboto.className} ${styles.body}`}>
